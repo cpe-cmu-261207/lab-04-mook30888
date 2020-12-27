223ec30de82c963b1f1a5d3fdc8490afe47415ea
mook30888 <mookkak@gmail.com>
2020-12-27 22:00:21 +0700

    all in

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import { useState } from "react";
+import {CourseCard} from "./components/CourseCard"
 
 function App() {
-  const grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F", "W"];
+  const grade = [{key: 'A', val: 4.0}, {key: 'B+', val: 3.5}, {key: 'B', val: 3.0},
+                 {key: 'C+', val: 2.5},{key: 'C', val: 2.0} , {key: 'D+', val: 1.5},
+                 {key: 'D', val: 1.0}, {key: 'F', val: 0.0}, {key: 'W', val: -1}];
   const credit = [1, 2, 3];
 
   const [myCourses, setMyCourse] = useState([]);
   const [inputData, setInputData] = useState({});
   const [GPA, setGPA] = useState(4.0);
 
+  let name = ""
+  let gradef = 0
+  let creditf =0
+  let a = ""
+
+
   /**
    * Calculate the GPA of current courses
    * @returns the GPA of current courses
    */
-  function calculateGPA() {
+  function calculateGPA(course) {
     // TODO
+    let gr = 0.00
+    let cr = 0
+    course.forEach(obj => {
+      if(Number(obj.cgrade) !== -1){
+        gr += Number(obj.cgrade) * Number(obj.ccredit)
+        cr += Number(obj.ccredit)
+      }
+    })
+    gr  = Number(gr) / Number(cr)
+    if(Number(gr) >= 0)
+      setGPA(Number(gr.toPrecision(3)))
+    else setGPA(0.00)
+
   }
 
   /**
@@ -24,9 +46,18 @@ function App() {
   function addCourse(event) {
     event.preventDefault();
     // TODO
+    const course ={
+      cname: name,
+      dis: a,
+      cgrade: gradef,
+      ccredit: creditf
+    }
+    const newCourse = [...myCourses,course]
+    setMyCourse(newCourse)
 
+    console.log(name +" "+ gradef +" " + creditf)
     // recalculate GPA
-    calculateGPA();
+    calculateGPA(newCourse);
   }
 
   /**
@@ -36,6 +67,10 @@ function App() {
    */
   function onDeleteCourse(id) {
     // TODO
+    const course = myCourses.filter(obj =>{
+      return obj.cname !== id
+    })
+    setMyCourse(course)
   }
 
   return (
@@ -46,8 +81,55 @@ function App() {
       <div className="h-2/3 md:w-2/4 p-3 rounded-lg mx-auto overflow-auto">
         <h1 className="text-2xl my-3">My courses</h1>
         {/* TODO display courses */}
+        {myCourses.map(item => {
+          return <CourseCard nn = {item.cname}
+          aa ={item.dis}
+          gg = {item.cgrade}
+          cc ={item.ccredit}
+          del = {onDeleteCourse} />
+          
+        })}
       </div>
+
       {/* TODO add course input form */}
+      <form onSubmit = {
+        e =>{addCourse(e)}
+      }>
+        <p className="text-center text-4xl p-3 tracking-widest" >
+        GPA = {GPA.toFixed(2)}
+      </p>
+      <div class = "divez">
+        <table>
+              <tr>
+                  <th>ชื่อวิชา</th>
+                  <th><input type = "text" placeholder = "ชื่อวิชา" onChange ={
+                  (e) => {name = e.currentTarget.value}
+                }/> </th>
+                  
+                
+                
+              </tr>
+              <tr>
+                <th>เกรด</th>
+                <th><select name = "Grade" onChange ={
+                  (e) => {gradef = e.currentTarget.value
+                          a = e.currentTarget.options[e.currentTarget.selectedIndex].text}}> <option> </option>{grade.map((item) =>{return (<option value={item.val}>{item.key}</option>)})}</select>
+                 </th>
+              </tr>
+              <tr>
+                <th>หน่วยกิต</th>
+                <th><select name = "Credit" onChange ={
+                  (e) => {creditf = e.currentTarget.value}}> <option> </option>{credit.map((item) =>{return (<option value={item}>{item}</option>)})}</select></th>
+                
+              </tr>
+              
+
+              </table>
+              
+            <button type="submit">submit</button>
+        </div>
+      </form>
+      
       {/* TODO display calculated GPA */}
     </div>
   );
--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,23 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return (
+    <>
+    <div class="CourseCard">
+      <tr >
+        <td>
+          <div class="tag">
+            <p>วิชา : {props.nn}</p>
+            <p>เกรด : {props.gg}</p>
+            <p>หน่วยกิต : {props.cc}</p>
+          </div>
+        </td>
+        <tr id="trc">
+          <button class="botn" onClick = {() =>{
+              props.del(props.nn)
+          }}>x</button>
+        </tr>
+      </tr>
+    </div>
+  </>
+  );
 };
